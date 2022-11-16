ecd43eb92f83ba15a66b4389a6864b250f6b8c58
alqama04 <alqamaazmi11@gmail.com>
2022-11-16 18:20:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Services from './pages/services/Services';
+import Layout from './pages/Layout';
+import Footer from './components/footer/Footer';
+import { useEffect } from 'react';
+function App() {
+  useEffect(() => {
+    const handleContextmenu = e => {
+        e.preventDefault()
+    }
+    document.addEventListener('contextmenu', handleContextmenu)
+    return function cleanup() {
+        document.removeEventListener('contextmenu', handleContextmenu)
+    }
+}, [ ])
+  return (
+    <BrowserRouter>
+     <Routes>
+     <Route path='/' element={<Layout />}>
+      <Route index element={<Home />}/>
+      <Route path='services' element={<Services />}/>
+      <Route path='about' element={<About />}/>
+     </Route>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
